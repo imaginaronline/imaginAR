a6181659594c79be92c7a1e15168c011613a03d4
imaginaronline <67681254+imaginaronline@users.noreply.github.com>
2022-10-27 11:16:56 +0530

    change

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
+      title: 'Flutter name',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
